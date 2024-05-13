74bcb496f7ca5523a849e0dc6cd2eadfd51f7a2a
Clara <clarafdezfraga@gmail.com>
2024-05-13 20:27:43 +0200

    Style main-container

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import MainContainer from "./components/MainContainer/MainContainer";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,7 @@
 const Header = (): React.ReactElement => {
   return (
     <header className="main-header">
-      <h1 className="main-title">The Why BLOG</h1>
+      <h1 className="main-title">The Why Blog</h1>
     </header>
   );
 };
