ce9b298f1d155bd0a217a1ee52c98dc973c7b525
Clara <clarafdezfraga@gmail.com>
2024-05-13 20:04:36 +0200

    Render header with web's title

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
-import "./App.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import MainContainer from "./components/MainContainer/MainContainer";
 
-function App() {}
-
-export default App;
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <MainContainer />
+  </React.StrictMode>,
+);
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+const Header = (): React.ReactElement => {
+  return (
+    <header className="main-header">
+      <h1 className="main-title">The Why BLOG</h1>
+    </header>
+  );
+};
+
+export default Header;
