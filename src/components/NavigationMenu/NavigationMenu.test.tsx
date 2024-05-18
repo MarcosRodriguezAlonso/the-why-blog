9bdb0f387f876a7d40936e10e61ae94dd3c6bf62
Clara <clarafdezfraga@gmail.com>
2024-05-18 19:34:59 +0200

    Fix pepinated pictures

--- src/components/NavigationMenu/NavigationMenu.test.tsx
+++ src/components/NavigationMenu/NavigationMenu.test.tsx
@@ -1,12 +1,21 @@
 import { render, screen } from "@testing-library/react";
 import NavigationMenu from "./NavigationMenu";
+import { MemoryRouter } from "react-router-dom";
+import App from "../App/App";
+
+beforeAll(() => {
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+});
 
 describe("Given the NavigationMenu component", () => {
   describe("When rendered", () => {
     test("Then it should show a link with 'Add'", () => {
       const expectedLinkText = /create post/i;
 
-      render(<NavigationMenu />);
       const link = screen.getByRole("navigation", { name: expectedLinkText });
 
       expect(link).toBeInTheDocument();
@@ -15,8 +24,9 @@ describe("Given the NavigationMenu component", () => {
     test("Then it should show a link with 'List'", () => {
       const expectedLinkText = /posts/i;
 
-      render(<NavigationMenu />);
-      const link = screen.getByRole("navigation", { name: expectedLinkText });
+      const link = screen.getByRole("link", {
+        name: "Posts",
+      });
 
       expect(link).toBeInTheDocument();
     });
