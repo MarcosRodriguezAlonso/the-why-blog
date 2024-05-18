d91168f153a902df6d7a5eaf4c3b59628ea8dd73
Clara <clarafdezfraga@gmail.com>
2024-05-18 16:50:02 +0200

    Test addNewPost returns post wtht Id

--- src/mocks/posts.ts
+++ src/mocks/posts.ts
@@ -1,13 +1,22 @@
-import { Post } from "../posts/types";
+import { Post, PostDataWithoutId } from "../posts/types";
+
+export const claraMockPostData: PostDataWithoutId = {
+  alternativeText: "una persona con el pelo largo",
+  imageUrl: "urldeclara",
+  author: "Clara",
+  title: "Porque el ornitorrinco no suda",
+  date: new Date(999999999999),
+  content: "El ornitorrinco suda leche",
+};
 
 export const claraMockPost: Post = {
   author: "Clara",
   content: "El ornitorrinco suda leche",
   title: "Porque el ornitorrinco no suda",
-  date: new Date(17147515),
+  date: new Date(999999999999),
   id: "clarapost",
-  imageUrl: "",
-  alternativeText: "",
+  imageUrl: "urldeclara",
+  alternativeText: "una persona con el pelo largo",
 };
 
 export const marcosMockPost: Post = {
