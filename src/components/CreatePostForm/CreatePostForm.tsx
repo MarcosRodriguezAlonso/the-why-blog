827244314f5dc5d3faf677cfa964c6cef8dbe825
Eric Lluis <ericlluis84@gmail.com>
2024-05-19 21:50:27 +0200

    Create styles to CreatePostForm

--- src/components/CreatePostForm/CreatePostForm.tsx
+++ src/components/CreatePostForm/CreatePostForm.tsx
@@ -3,28 +3,28 @@ import "./CreatePostForm.scss";
 const CreatePostForm = (): React.ReactElement => {
   return (
     <form className="form">
-      <div className="form__container" contentEditable="true">
-        <h2>Create Post</h2>
-        <label htmlFor="author" className="form__author">
-          Author:{" "}
-        </label>
-        <input id="author" name="author" autoFocus={true}></input>
-        <label htmlFor="title" className="form__title" autoFocus>
-          Title:{" "}
-        </label>
-        <input id="title" name="title"></input>
-        <label htmlFor="content" className="form__content">
-          Content:{" "}
-        </label>
-        <input id="content" name="content"></input>
-        <label htmlFor="image" className="form__image">
-          Image Url:{" "}
-        </label>
-        <input id="image" name="image"></input>
-        <button onClick={CreatePostForm} className="form__button">
-          👉 Submit
-        </button>
-      </div>
+      <h2>Create Post</h2>
+      <label htmlFor="author" className="form__author">
+        Author:{" "}
+      </label>
+      <input id="author" />
+      <label htmlFor="title" className="form__title">
+        Title:{" "}
+      </label>
+      <input id="title" />
+      <label htmlFor="content" className="form__content">
+        Content:{" "}
+      </label>
+      <input id="content" />
+      <label htmlFor="image" className="form__image">
+        Image Url:{" "}
+      </label>
+      <input id="image" />
+      <label htmlFor="alternative-text" className="form__alternative-text">
+        Alternative Text:{" "}
+      </label>
+      <input id="alternative-text" />
+      <button className="form__button">👉 Save Post</button>
     </form>
   );
 };
