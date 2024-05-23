a75ca448ab5e4847fd8237989053d1b187e00c47
Clara <clarafdezfraga@gmail.com>
2024-05-23 22:29:18 +0200

    Connect imput to its value

--- src/components/CreatePostForm/CreatePostForm.tsx
+++ src/components/CreatePostForm/CreatePostForm.tsx
@@ -1,33 +1,70 @@
+import { useState } from "react";
 import "./CreatePostForm.scss";
 
 const CreatePostForm = (): React.ReactElement => {
+  const [author, setAuthor] = useState("");
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+  const [alternativeText, setAlternativeText] = useState("");
+
   return (
     <form className="form" autoComplete="off">
       <div className="form__group">
         <label htmlFor="author">Author: </label>
-        <input className="form__input" id="author" type="text" />
+        <input
+          value={author}
+          onChange={(event) => setAuthor(event.target.value)}
+          className="form__input"
+          id="author"
+          type="text"
+        />
       </div>
       <div className="form__group">
         <label htmlFor="title">Title: </label>
-        <input className="form__input" id="title" type="text" />
+        <input
+          value={title}
+          onChange={(event) => setTitle(event.target.value)}
+          className="form__input"
+          id="title"
+          type="text"
+        />
       </div>
       <div className="form__group">
         <label htmlFor="content" className="form__content">
           Content:
         </label>
-        <input className="form__input" id="content" type="text" />
+        <input
+          value={content}
+          onChange={(event) => setContent(event.target.value)}
+          className="form__input"
+          id="content"
+          type="text"
+        />
       </div>
       <div className="form__group">
-        <label htmlFor="image" className="form__image">
+        <label htmlFor="imageUrl" className="form__image">
           Image Url:
         </label>
-        <input className="form__input" id="image" type="url" />
+        <input
+          value={imageUrl}
+          onChange={(event) => setImageUrl(event.target.value)}
+          className="form__input"
+          id="imageUrl"
+          type="url"
+        />
       </div>
       <div className="form__group">
-        <label htmlFor="alternative-text" className="form__alternative-text">
-          Alternative Text:{" "}
+        <label htmlFor="alternativeText" className="form__alternative-text">
+          Alternative Text:
         </label>
-        <input className="form__input" id="alternative-text" type="text" />
+        <input
+          value={alternativeText}
+          onChange={(event) => setAlternativeText(event.target.value)}
+          className="form__input"
+          id="alternativeText"
+          type="text"
+        />
       </div>
       <button className="form__button">👉 Save Post</button>
     </form>
